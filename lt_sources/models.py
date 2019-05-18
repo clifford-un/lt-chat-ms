cfb185c55414cbd75883236ddb15b13c2b35eb0d
Juanito Becerra <jsbecerrab@unal.edu.co>
2019-05-17 21:21:42 -0500

    model updates, DELETE Method

--- lt_sources/models.py
+++ lt_sources/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from mongoengine import Document, fields
+import datetime
 
 class Chat(Document):
     chat_user_origin = fields.IntField(required=True)
-    chat_group_destination = fields.IntField(required=True)
+    chat_room_id = fields.IntField(required=True)
     chat_text = fields.StringField(requiered=True)
-    #chat_date_stamp=models.DateTimeField(auto_now_add=True)
-    
-
+    chat_hidden = fields.BooleanField(default=False)
+    chat_date_stamp = fields.DateTimeField(default=datetime.datetime.today)
     
--- lt_sources/views.py
+++ lt_sources/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 from .models import Chat
 from .serializers import ChatSerializer
 
 class ChatView(APIView):
 
-    def get(self, request):
-        serializer = ChatSerializer(Chat.objects.all(), many=True)
+    def get_object(self):
+        try:
+            return Chat.objects.filter(chat_hidden=False)
+        except Chat.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        serializer = ChatSerializer(self.get_object(), many=True)
         response = {"Chats": serializer.data}
         return Response(response, status=status.HTTP_200_OK)
 
@@ -23,7 +30,21 @@ class ChatView(APIView):
 
 class ChatViewDetail(APIView):
 
-    def get(self, request,id):
-        serializer = ChatSerializer(Chat.objects.get(id=id))
+    def get_object(self, id):
+        try:
+            return Chat.objects.get(id=id)
+        except Chat.DoesNotExist:
+            raise Http404
+
+    def get(self, request,id, format=None):
+        serializer = ChatSerializer(self.get_object(id))
         response = {"Chats": serializer.data}
         return Response(response, status=status.HTTP_200_OK)
+
+    def delete(self, request, id, format=None):
+        chat = self.get_object(id)
+        chat.chat_hidden=True
+        chat.save(update_fields=["chat_hidden"])
+        response = "mensaje eliminado"
+        return Response(response, status=status.HTTP_204_NO_CONTENT) 
+
