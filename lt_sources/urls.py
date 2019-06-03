2a169be7865df2c781b5082afa21e53146727589
Juanito Becerra <jsbecerrab@unal.edu.co>
2019-06-03 11:58:33 -0500

    test 2

--- lt_sources/urls.py
+++ lt_sources/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.ChatView.as_view()),
+    path('chats/', views.ChatView.as_view()),
     path('chats/<str:chat_room_id>/', views.ChatViewDetail.as_view()),
-    path('chat/<str:id>/', views.ChatViewDelete.as_view()),
+    path('chat/d/<str:id>/', views.ChatViewDelete.as_view()),
 ]
