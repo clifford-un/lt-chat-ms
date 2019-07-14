b05cd7ce57a5480e58e47872391e5d732825f884
Juanito Becerra <36463265+jsbecerrab@users.noreply.github.com>
2019-07-14 06:53:18 -0500

    db update

--- lt_chat/settings.py
+++ lt_chat/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MONGO_DATABASE_NAME = 'lt_chat'
-MONGO_HOST = "lt-chat-db-2"
+MONGO_HOST = "kwii-lt-chat-db-2"
 MONGO_PORT = 27017
 connect(host=MONGO_HOST)
